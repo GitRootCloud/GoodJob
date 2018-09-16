92071281263d0bfb68add94b0f16157253844041
chen <subingchen@aliyun.com>
2018-09-16 14:48:42 +0800

    1.0x

--- goodjob/user/views.py
+++ goodjob/user/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def login_views(request):
+  # return HttpResponse('你好')
+  return render(request,'user_register.html')
